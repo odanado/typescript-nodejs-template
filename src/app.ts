61679035c57fdb31bdd4b8a754f79681143d9bfb
odan <odan3240@gmail.com>
2020-04-04 23:08:21 +0900

    enable decorator

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-process.on("unhandledRejection", reason => {
-  console.error(reason);
-  process.exit(1);
-});
-
 async function main(): Promise<void> {
   console.log("poyo");
 }
 
 main();
+
+process.on("unhandledRejection", reason => {
+  console.error(reason);
+  process.exit(1);
+});
