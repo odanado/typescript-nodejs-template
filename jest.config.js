93e09375fffc953af36b192333da06312fd8d661
odan <odan3240@gmail.com>
2021-09-19 21:49:39 +0900

    add esbuild-jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "ts-jest",
   collectCoverage: true,
   coverageReporters: ["json", "lcov", "text", "html"],
+  transform: {
+    "^.+\\.tsx?$": "esbuild-jest",
+  },
 };
