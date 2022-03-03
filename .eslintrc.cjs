6b649724bce990b0b9b608376720f01b461d89d7
odan <odan3240@gmail.com>
2022-03-03 23:52:51 +0900

    chore: rename file of eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,15 +7,11 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:jest/all",
     "prettier",
   ],
-  plugins: ["jest", "@typescript-eslint"],
+  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
-  },
-  rules: {
-    "jest/require-hook": "off",
   },
 };
